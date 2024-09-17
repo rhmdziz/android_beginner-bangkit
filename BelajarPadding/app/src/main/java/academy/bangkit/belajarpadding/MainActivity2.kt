c3039498238c04b839572ffafe2da193683bdc98
rhmdziz <rahmadaziz288@gmail.com>
2024-09-17 16:26:34 +0700

    intent with data

---
+++ BelajarPadding/app/src/main/java/academy/bangkit/belajarpadding/MainActivity2.kt
@@ -0,0 +1,30 @@
+package academy.bangkit.belajarpadding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main2)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(
+//                systemBars.left + v.paddingLeft,
+//                systemBars.top + v.paddingTop,
+//                systemBars.right + v.paddingRight,
+//                systemBars.bottom + v.paddingBottom
+//            )
+//            insets
+//        }
+    }
+}
