77f1a9264907857b09543a7b8489c1e9ebd41721
rhmdziz <rahmadaziz288@gmail.com>
2024-10-06 23:44:53 +0700

    Navigation Drawer with Fragment and Activity

---
+++ MyNavigationDrawer/app/src/main/java/academy/bangkit/mynavigationdrawer/SubwayActivity.kt
@@ -0,0 +1,15 @@
+package academy.bangkit.mynavigationdrawer
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+
+    }
+}
